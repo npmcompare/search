424fec80a11afe71472a99f800f48c2e34c1b964
Daniel Cohen <danielc@promots.tv>
2016-07-26 15:40:14 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const validate = require("validate-npm-package-name");
+const app = require('express')();
+const got = require('got');
+const memoize = require('memoizee');
+const config = require('rc')('npmcompare');
+
+const search = memoize(q => {
+    return new Promise((resolve, reject) => {
+        console.log(`Search: ${q}`);
+        let url = `https://ac.cnstrc.com/autocomplete/${q}?autocomplete_key=${config.npm_autocomplete_key}&query=${q}`;
+        got(url, {json: true})
+            .then(result => {
+                let i = result.body.sections.packages.length;
+                let results = result.body.sections.packages.map(pkg => {
+                    return {
+                        name: pkg.value,
+                        description: pkg.data.description,
+                        priority: --i
+                    };
+                });
+                resolve(results);
+            }, reject);
+    });
+}, {
+    promise: true,
+    maxAge: parseInt(config.search_cache_timeout || 6 * 60 * 60 * 1000) // Default 6 hours
+});
+
+app.get('/', (req, res) => res.send('npmcompare search'));
+
+app.get('/:query', (req, res) => {
+    let q = req.params.query.toLowerCase();
+    let valid = validate(q);
+    if (!q || !valid.validForNewPackages) return res.status(400).json('invalid package name provided');
+
+    search(req.params.query.toLowerCase()).then(results => {
+        res.json(results);
+    }, err => {
+        res.status(400).json('an error occurred');
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Search microservice listening on port ${port}`));
